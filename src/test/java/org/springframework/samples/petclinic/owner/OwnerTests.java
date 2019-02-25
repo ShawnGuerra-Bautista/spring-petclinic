19b50aaeb18b56effb8c83a137a198bacfda1314
Nirmal <nirmal_864@hotmail.com>
2019-02-25 14:46:38 -0500

    #1 added test file for owner

---
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerTests.java
@@ -0,0 +1,17 @@
+package org.springframework.samples.petclinic.owner;
+
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+
+public class OwnerTests{
+
+    @Test
+    public void testGetPets()
+    {
+      
+    }
+
+
+}
