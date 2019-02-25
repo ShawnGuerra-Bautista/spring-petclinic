6222c4d7da7b425bbebff633752204d612d37677
Rez97 <rezhand97@gmail.com>
2019-02-25 17:29:04 -0500

    [#5] statically imported the Mockito library

--- src/test/java/org/springframework/samples/petclinic/owner/PetValidatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetValidatorTest.java
@@ -3,7 +3,7 @@ package org.springframework.samples.petclinic.owner;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 import org.springframework.validation.Errors;
 
 public class PetValidatorTest {
@@ -14,15 +14,15 @@ public class PetValidatorTest {
 	  @Test
 	    public void ValidatWhenBirthdayDateIsNull() {
 	    	PetValidator petValidate=new PetValidator();
-	    	Pet pet=Mockito.mock(Pet.class);
-	    	PetType petType=Mockito.mock(PetType.class);
-	    	Errors error=Mockito.mock(Errors.class);
-	    	Mockito.when(pet.getName()).thenReturn("Garfield");
-	    	Mockito.when(pet.getId()).thenReturn(1);
-	    	Mockito.when(pet.getType()).thenReturn(petType);
-	    	Mockito.when(pet.getBirthDate()).thenReturn(null);
+	    	Pet pet=mock(Pet.class);
+	    	PetType petType=mock(PetType.class);
+	    	Errors error=mock(Errors.class);
+	    	when(pet.getName()).thenReturn("Garfield");
+	    	when(pet.getId()).thenReturn(1);
+	    	when(pet.getType()).thenReturn(petType);
+	    	when(pet.getBirthDate()).thenReturn(null);
 	    	petValidate.validate(pet, error);
-	    	Mockito.verify(error).rejectValue("birthDate","required","required");
+	    	verify(error).rejectValue("birthDate","required","required");
 	    }
 	  
 	  /**
@@ -32,11 +32,11 @@ public class PetValidatorTest {
 	  @Test
 	  public void ValidateWhenNameIsUndefined() {
 		  PetValidator petValidate=new PetValidator();
-	      Pet pet=Mockito.mock(Pet.class);
-	      Errors error=Mockito.mock(Errors.class);
+	      Pet pet=mock(Pet.class);
+	      Errors error=mock(Errors.class);
 	      petValidate.validate(pet, error);
-	      Mockito.verify(pet).getName();
-	      Mockito.verify(error).rejectValue("name","required","required");   
+	      verify(pet).getName();
+	      verify(error).rejectValue("name","required","required");   
 	      
 	  }
 
