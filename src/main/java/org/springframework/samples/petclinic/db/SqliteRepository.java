bf52fec1efb94d02e67d26ad3090b3ccd84c5d85
Refat Kamal <refat.k17@gmail.com>
2019-03-25 12:58:09 -0400

    #19 abstract the Sqlite repo

---
+++ src/main/java/org/springframework/samples/petclinic/db/SqliteRepository.java
@@ -0,0 +1,70 @@
+package org.springframework.samples.petclinic.db;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public abstract class SqliteRepository<T extends BaseEntity> {
+
+    public static String asSqlString(String string) {
+        return "\"" + string + "\"";
+    }
+
+    public abstract String getTableName();
+    public abstract String getAllColumns();
+    public abstract String toValues(T entity);
+
+    public String allToValues(List<T> entities) {
+        StringBuilder values = new StringBuilder();
+
+        for(T entity : entities) {
+            values.append(toValues(entity)).append(", ");
+        }
+
+        values.replace(values.length() - 2, values.length(), "");
+        return values.toString();
+    }
+
+    /**
+     * Pass any Sqlite Query to make a change in the DB
+     * @param query The query to execute
+     * @param type Name the query for logging
+     */
+    public void executeUpdate(String query, String type) {
+        Connection con = SQLiteDatabase.getConnection();
+        Statement statement;
+        try {
+            statement = con.createStatement();
+            System.out.println("Running " + type +" Query... \n" + query);
+            statement.executeUpdate(query);
+            System.out.println(type + " Successful");
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Saves one DB entity row. If new, it's added. If existing, it's replaced
+     * @param entity is saved
+     */
+    public void save(T entity) {
+        String saveQuery = "INSERT OR REPLACE INTO " + getTableName() + " " + getAllColumns()
+                         + " VALUES " + toValues(entity);
+        executeUpdate(saveQuery, "Save to " + getTableName());
+    }
+
+    /**
+     * Like save, but for a collection of DB entities
+     * @param entities are saved
+     */
+    public void saveAll(List<T> entities) {
+        String saveAllQuery = "INSERT OR REPLACE INTO " + getTableName() + " " + getAllColumns()
+            + " VALUES " + allToValues(entities);
+        executeUpdate(saveAllQuery, "Save All");
+    }
+
+}
--- src/main/java/org/springframework/samples/petclinic/owner/OwnerSqliteRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerSqliteRepository.java
@@ -1,18 +1,13 @@
 package org.springframework.samples.petclinic.owner;
 
-import org.springframework.samples.petclinic.db.SQLiteDatabase;
+import org.springframework.samples.petclinic.db.SqliteRepository;
 import org.springframework.stereotype.Repository;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-import java.sql.Statement;
 
 
 @Repository
-public class OwnerSqliteRepository {
+public class OwnerSqliteRepository extends SqliteRepository<Owner> {
 
-    public static final String TABLE_NAME = "owners";
+    public final String TABLE_NAME = "owners";
 
     enum Column {
         id,
@@ -23,7 +18,11 @@ public class OwnerSqliteRepository {
         telephone
     }
 
-    public static String getAllColumns() {
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+
+    public String getAllColumns() {
 
         StringBuilder columns = new StringBuilder("(");
 
@@ -35,11 +34,7 @@ public class OwnerSqliteRepository {
         return columns.toString();
     }
 
-    private static String asSqlString(String string) {
-        return "\"" + string + "\"";
-    }
-
-    public static String toValues(Owner owner) {
+    public String toValues(Owner owner) {
         return "(" +
             owner.getId() + ", " +
             asSqlString(owner.getFirstName()) + ", " +
@@ -48,44 +43,5 @@ public class OwnerSqliteRepository {
             asSqlString(owner.getCity()) + ", " +
             asSqlString(owner.getTelephone()) +
             ")";
-    }
-
-    public static String allToValues(List<Owner> owners) {
-        StringBuilder values = new StringBuilder();
-
-        for(Owner owner : owners) {
-            values.append(toValues(owner)).append(", ");
-        }
-
-        values.replace(values.length() - 2, values.length(), "");
-        return values.toString();
-    }
-
-    public void save(Owner owner) {
-        Connection con = SQLiteDatabase.getConnection();
-        Statement statement;
-        try {
-            statement = con.createStatement();
-            String insertQuery = "INSERT OR REPLACE INTO " + TABLE_NAME + " " + getAllColumns() + " VALUES " + toValues(owner);
-            System.out.println("Running Save All Query... \n" + insertQuery);
-            statement.executeUpdate(insertQuery);
-            con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void saveAll(List<Owner> owners) {
-        Connection con = SQLiteDatabase.getConnection();
-        Statement statement;
-        try {
-            statement = con.createStatement();
-            String insertAllQuery = "INSERT OR REPLACE INTO " + TABLE_NAME + " " + getAllColumns() + " VALUES " + allToValues(owners);
-            System.out.println("Running Save All Query... \n" + insertAllQuery);
-            statement.executeUpdate(insertAllQuery);
-            con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
