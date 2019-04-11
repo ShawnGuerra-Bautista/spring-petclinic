799836aef45b93b522f4e1f88b2f6d354867f9e9
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-04-10 21:53:05 -0400

    [#29] Added new parameter into the layout

--- src/main/java/org/springframework/samples/petclinic/system/PetClinicToggles.java
+++ src/main/java/org/springframework/samples/petclinic/system/PetClinicToggles.java
@@ -1,10 +1,8 @@
 package org.springframework.samples.petclinic.system;
 
-import java.util.Random;
-
 public interface PetClinicToggles {
     int MIN = 0;
     int MAX = 1;
-    public static Boolean toggleFindOwnerByLastName = (int)((Math.random() * (MAX - MIN + 1)) + MIN) == 0;
-    public static Boolean toggleListOfOwners = (int)((Math.random() * (MAX - MIN + 1)) + MIN) == 0;
+    public static final Boolean toggleFindOwnerByLastName = (int)((Math.random() * (MAX - MIN + 1)) + MIN) == 0;
+    public static final Boolean toggleListOfOwners = (int)((Math.random() * (MAX - MIN + 1)) + MIN) == 0;
 }
