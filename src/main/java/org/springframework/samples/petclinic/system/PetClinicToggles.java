565108f82da309af28e813a2dc4b681a29589c7e
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-04-10 21:24:54 -0400

    [#29] Toggles are random

--- src/main/java/org/springframework/samples/petclinic/system/PetClinicToggles.java
+++ src/main/java/org/springframework/samples/petclinic/system/PetClinicToggles.java
@@ -1,6 +1,10 @@
 package org.springframework.samples.petclinic.system;
 
+import java.util.Random;
+
 public interface PetClinicToggles {
-    Boolean toggleFindOwnerByLastName = false;
-    Boolean toggleListOfOwners = false;
+    int MIN = 0;
+    int MAX = 1;
+    public static Boolean toggleFindOwnerByLastName = (int)((Math.random() * (MAX - MIN + 1)) + MIN) == 0;
+    public static Boolean toggleListOfOwners = (int)((Math.random() * (MAX - MIN + 1)) + MIN) == 0;
 }
