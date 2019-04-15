ad834abe80b8f71ac1d600e55bd99fe1883174d4
Refat Kamal <refat.k17@gmail.com>
2019-04-15 01:54:05 -0400

    [#29] created test to collect fake data

--- src/test/java/org/springframework/samples/petclinic/toggles/ListOfOwnerToggleTests.java
+++ src/test/java/org/springframework/samples/petclinic/toggles/ListOfOwnerToggleTests.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.petclinic.toggles;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +14,7 @@ import org.springframework.samples.petclinic.system.PetClinicToggles;
 import org.springframework.validation.BindingResult;
 
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.mockito.Mockito.*;
 
@@ -86,6 +88,70 @@ public class ListOfOwnerToggleTests {
         verify(mockOwnerListCsvLogger).info("0");
         // shouldn't have happened anymore
         verify(mockOwnerListCsvLogger, times(1)).info("1");
+    }
+
+    @Test
+    public void collectFakeData() {
+        Logger realCsvLogger = LogManager.getLogger("listOfOwner");
+        OwnerController ownerController = new OwnerController(mockOwnerRepository, mockConsoleLogger, realCsvLogger);
+
+        // set up so that around half users get new feature
+        PetClinicToggles.toggleListOfOwners = new Toggle(0.5f);
+
+        int numUsers = 50;
+        int numToggleOnUsers = 0;
+
+        int numToggleOn = 0;
+        int numToggleOff = 0;
+
+        // do data gather
+        for (int i = 0; i < numUsers; i++) {
+            // randomly set the toggle
+            PetClinicToggles.toggleListOfOwners.setToggleUsingRolloutRatio();
+
+            int visits;
+
+            // when toggle on, more visits likely
+            if (PetClinicToggles.toggleListOfOwners.isOn()) {
+                numToggleOnUsers++;
+
+                visits = getWeightedRandomNumber(10, 2, 0, 15);
+                for (int j = 0; j < visits; j++) {
+                    numToggleOn++;
+                    ownerController.showOwnerList(mockModel);
+                }
+            } else {
+                visits = getWeightedRandomNumber(0, 2, 0, 5);
+                for (int j = 0; j < visits; j++) {
+                    numToggleOff++;
+                    ownerController.processFindForm(mockOwner, mockResult, mockModel);
+                }
+            }
+        }
+
+        Logger logTotals = LogManager.getLogger("trace");
+        logTotals.info("Num users = " + numUsers);
+        logTotals.info("Num toggle on Users = " + numToggleOnUsers);
+        logTotals.info("Num toggle of Users = " + (numUsers - numToggleOnUsers));
+
+        logTotals.info("n = " + (numToggleOn + numToggleOff));
+        logTotals.info("Num toggle on visits = " + numToggleOn);
+        logTotals.info("Num toggle off visits = " + (numToggleOff));
+    }
+
+    private int getWeightedRandomNumber(int weightPosition, double standardDeviation, int leftBound, int rightBound) {
+
+        int rand = (int)(ThreadLocalRandom.current().nextGaussian() * standardDeviation + weightPosition);
+
+        if (rand < leftBound) {
+            rand = leftBound;
+        }
+
+        if (rand > rightBound) {
+            rand = rightBound;
+        }
+
+        return rand;
     }
 
 }
