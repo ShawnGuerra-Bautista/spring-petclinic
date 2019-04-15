36370aa0eb006e8ebab5cc1d0a97047ebbb63c64
omarhammoud <omar_hammoud96@hotmail.com>
2019-04-15 15:46:31 -0400

    added toggles

--- src/main/java/org/springframework/samples/petclinic/system/PetClinicToggles.java
+++ src/main/java/org/springframework/samples/petclinic/system/PetClinicToggles.java
@@ -1,4 +1,15 @@
 package org.springframework.samples.petclinic.system;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 public interface PetClinicToggles {
+    int MIN = 0;
+    int MAX = 1;
+    public static final Boolean toggleFindOwnerByLastName = (int)((Math.random() * (MAX - MIN + 1)) + MIN) == 0;
+    public static final Boolean toggleListOfOwners = (int)((Math.random() * (MAX - MIN + 1)) + MIN) == 0;
+    public static final Boolean toggleSearchByLocation = (int)((Math.random() * (MAX - MIN + 1)) + MIN) == 0;
+    public static final Boolean toggleSearchByTelephone = (int)((Math.random() * (MAX - MIN + 1)) + MIN) == 0;
+    public static final Collection<Boolean> toggles = new ArrayList<Boolean>(Arrays.asList(toggleFindOwnerByLastName, toggleListOfOwners, toggleSearchByLocation));
 }
